8a04aadc37f0ecca9aacdb99d53424767b1720ba
Yiming Zhang <61449077+swift-zym@users.noreply.github.com>
2020-10-02 23:52:15 +0000

    fix data

--- zr-Day1-fix/run.cpp
+++ zr-Day1-fix/run.cpp
@@ -1,13 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    system("g++ std.cpp -o std.exe -std=c++17");
+    system("g++ zr-Day1-fix/std.cpp -o zr-Day1-fix/std -std=c++17");
     for(int i=1;i<=10;i++){
-        //string cmd="start std.exe >"+to_string(i)+".in <"+to_string(i)+".out";
-        string cmd="start cmd.exe";
+        string cmd="touch zr-Day1-fix/"+to_string(i)+".out";
+        cout<<cmd<<endl;
         system(cmd.c_str());
-        system("dir");
-        system("pause");
+        cmd="./zr-Day1-fix/std <zr-Day1-fix/"+to_string(i)+".in >zr-Day1-fix/"+to_string(i)+".out";
+        //string cmd="start cmd.exe";
+        cout<<cmd<<endl;
+        system(cmd.c_str());
     }
     return 0;
 }
